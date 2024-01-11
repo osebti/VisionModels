574e7af8f4b6b44d317f4f95c64f8ea7ee14adc3
osebti <91134164+osebti@users.noreply.github.com>
2024-01-11 10:03:51 -0700

    ae and vae with pretrained model files

---
+++ Diffusion/A5_diffusion_submission.py
@@ -0,0 +1,310 @@
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+from typing import Tuple
+
+"""
+NOTE: you can add as many functions as you need in this file, and for all the classes you can define extra methods if you need
+"""
+
+def sinusoidal_embedding(n, d):
+  # Returns the standard positional embedding
+  embedding = torch.zeros(n, d)
+  wk = torch.tensor([1 / 10_000 ** (2 * j / d) for j in range(d)])
+  wk = wk.reshape((1, d))
+  t = torch.arange(n).reshape((n, 1))
+  embedding[:,::2] = torch.sin(t * wk[:,::2])
+  embedding[:,1::2] = torch.cos(t * wk[:,::2])
+
+  return embedding
+
+
+
+class MyBlock(nn.Module):
+  def __init__(self, shape, in_c, out_c, kernel_size=3, stride=1, padding=1, activation=None, normalize=True):
+    super(MyBlock, self).__init__()
+    self.ln = nn.LayerNorm(shape)
+    self.conv1 = nn.Conv2d(in_c, out_c, kernel_size, stride, padding)
+    self.conv2 = nn.Conv2d(out_c, out_c, kernel_size, stride, padding)
+    self.activation = nn.SiLU() if activation is None else activation
+    self.normalize = normalize
+
+  def forward(self, x):
+    out = self.ln(x) if self.normalize else x
+    out = self.conv1(out)
+    out = self.activation(out)
+    out = self.conv2(out)
+    out = self.activation(out)
+    return out
+
+
+
+class VarianceScheduler:
+  """
+  This class is used to keep track of statistical variables used in the diffusion model
+  and also adding noise to the data
+  """
+  def __init__(self, beta_start: float=0.0001, beta_end: float=0.02, num_steps :int=1000):
+    device= "cuda" if torch.cuda.is_available() else "cpu"
+    self.betas = torch.linspace(beta_start, beta_end, num_steps)  # Number of steps is typically in the order of thousands
+    self.alphas = 1 - self.betas
+    self.alpha_bars = torch.tensor([torch.prod(self.alphas[:i + 1]) for i in range(len(self.alphas))])
+    self.steps=num_steps
+    
+        # NOTE:Feel free to add to this or modify it as you wish
+
+  
+  
+  def add_noise(self, x: torch.Tensor, timestep: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+    """
+    This method receives the input data and the timestep, generates a noise according to the 
+    timestep, perturbs the data with the noise, and returns the noisy version of the data and
+    the noise itself
+    
+    Args:
+      x (torch.Tensor): input image [B, 1, 28, 28]
+      timestep (torch.Tensor): timesteps [B]
+
+    Returns:
+      Tuple[torch.Tensor, torch.Tensor]: noisy_x [B, 1, 28, 28], noise [B, 1, 28, 28]
+    """
+
+    device="cuda" if torch.cuda.is_available() else "cpu"
+    eta = torch.randn(x.shape)
+    noisy_x=torch.zeros(x.shape)
+    n=int(x.shape[0])
+
+    if len(timestep.shape)==0:
+      timestep=torch.tensor([timestep]).type(torch.int64)
+    
+    x=x.cpu()
+    
+    for i in range(n):
+      a_bar = self.alpha_bars[timestep[i]]
+      noisy_x[i] = a_bar.sqrt().reshape(1, 1, 1, 1) * x[i] + (1 - a_bar).sqrt().reshape(1, 1, 1, 1) * eta[i]
+
+
+    return noisy_x, eta
+    
+
+
+class NoiseEstimatingNet(nn.Module):
+  """
+  The implementation of the noise estimating network for the diffusion model
+  """
+  # feel free to add as many arguments as you need or change the arguments
+  def __init__(self, time_emb_dim: int=64, num_classes: int=10, n_steps=1000):
+  
+    super(NoiseEstimatingNet, self).__init__()
+
+    # Sinusoidal embedding
+    
+    self.time_embed = nn.Embedding(n_steps, time_emb_dim)
+    self.time_embed.weight.data = sinusoidal_embedding(n_steps, time_emb_dim)
+    self.time_embed.requires_grad_(False)
+    self.class_embed = nn.Embedding(num_classes,time_emb_dim)
+    self.steps=n_steps
+    #self.time_embed.requires_grad_(False)
+
+    # First half
+    self.te1 = self._make_te(time_emb_dim, 1)
+    self.b1 = nn.Sequential(
+      MyBlock((1, 28, 28), 1, 10),
+      MyBlock((10, 28, 28), 10, 10),
+      MyBlock((10, 28, 28), 10, 10)
+    )
+    self.down1 = nn.Conv2d(10, 10, 4, 2, 1)
+
+    self.te2 = self._make_te(time_emb_dim, 10)
+    self.b2 = nn.Sequential(
+      MyBlock((10, 14, 14), 10, 20),
+      MyBlock((20, 14, 14), 20, 20),
+      MyBlock((20, 14, 14), 20, 20)
+    )
+    self.down2 = nn.Conv2d(20, 20, 4, 2, 1)
+
+    self.te3 = self._make_te(time_emb_dim, 20)
+    self.b3 = nn.Sequential(
+      MyBlock((20, 7, 7), 20, 40),
+      MyBlock((40, 7, 7), 40, 40),
+      MyBlock((40, 7, 7), 40, 40)
+    )
+    self.down3 = nn.Sequential(
+      nn.Conv2d(40, 40, 2, 1),
+      nn.SiLU(),
+      nn.Conv2d(40, 40, 4, 2, 1)
+    )
+
+    # Bottleneck
+    self.te_mid = self._make_te(time_emb_dim, 40)
+    self.b_mid = nn.Sequential(
+      MyBlock((40, 3, 3), 40, 20),
+      MyBlock((20, 3, 3), 20, 20),
+      MyBlock((20, 3, 3), 20, 40)
+    )
+
+    # Second half
+    self.up1 = nn.Sequential(
+      nn.ConvTranspose2d(40, 40, 4, 2, 1),
+      nn.SiLU(),
+      nn.ConvTranspose2d(40, 40, 2, 1)
+    )
+
+    self.te4 = self._make_te(time_emb_dim, 80)
+    self.b4 = nn.Sequential(
+      MyBlock((80, 7, 7), 80, 40),
+      MyBlock((40, 7, 7), 40, 20),
+      MyBlock((20, 7, 7), 20, 20)
+    )
+
+    self.up2 = nn.ConvTranspose2d(20, 20, 4, 2, 1)
+    self.te5 = self._make_te(time_emb_dim, 40)
+    self.b5 = nn.Sequential(
+      MyBlock((40, 14, 14), 40, 20),
+      MyBlock((20, 14, 14), 20, 10),
+      MyBlock((10, 14, 14), 10, 10)
+    )
+
+    self.up3 = nn.ConvTranspose2d(10, 10, 4, 2, 1)
+    self.te_out = self._make_te(time_emb_dim, 20)
+    self.b_out = nn.Sequential(
+      MyBlock((20, 28, 28), 20, 10),
+      MyBlock((10, 28, 28), 10, 10),
+      MyBlock((10, 28, 28), 10, 10, normalize=False)
+    )
+
+    self.conv_out = nn.Conv2d(10, 1, 3, 1, 1)
+
+  def forward(self, x: torch.Tensor, timestep: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+  
+    # x is (N, 2, 28, 28) (image with positional embedding stacked on channel dimension)
+    t = self.time_embed(timestep)
+    c= self.class_embed(y.unsqueeze(1))
+    t=t+c
+    n = len(x)
+    out1 = self.b1(x + self.te1(t).reshape(n, -1, 1, 1))  # (N, 10, 28, 28)
+    out2 = self.b2(self.down1(out1) + self.te2(t).reshape(n, -1, 1, 1))  # (N, 20, 14, 14)
+    out3 = self.b3(self.down2(out2) + self.te3(t).reshape(n, -1, 1, 1))  # (N, 40, 7, 7)
+
+    out_mid = self.b_mid(self.down3(out3) + self.te_mid(t).reshape(n, -1, 1, 1))  # (N, 40, 3, 3)
+
+    out4 = torch.cat((out3, self.up1(out_mid)), dim=1)  # (N, 80, 7, 7)
+    out4 = self.b4(out4 + self.te4(t).reshape(n, -1, 1, 1))  # (N, 20, 7, 7)
+
+    out5 = torch.cat((out2, self.up2(out4)), dim=1)  # (N, 40, 14, 14)
+    out5 = self.b5(out5 + self.te5(t).reshape(n, -1, 1, 1))  # (N, 10, 14, 14)
+
+    out = torch.cat((out1, self.up3(out5)), dim=1)  # (N, 20, 28, 28)
+    out = self.b_out(out + self.te_out(t).reshape(n, -1, 1, 1))  # (N, 1, 28, 28)
+
+    out = self.conv_out(out)
+
+    return out
+  
+  def _make_te(self, dim_in, dim_out):
+    return nn.Sequential(
+      nn.Linear(dim_in, dim_out),
+      nn.SiLU(),
+      nn.Linear(dim_out, dim_out)
+    )
+
+
+
+class DiffusionModel(nn.Module):
+  """
+  The whole diffusion model put together
+  """
+  def __init__(self, network: nn.Module, var_scheduler: VarianceScheduler):
+
+    super().__init__()
+    
+    self.network = network # UNet
+    self.var_scheduler = var_scheduler
+  
+  def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.float32:
+
+    
+    # step1: sample timesteps
+    # step2: compute the noisy versions of the input image according to your timesteps
+    # step3: estimate the noises using your noise estimating network
+    # step4: compute the loss between the estimated noises and the true noises
+
+    device="cuda" if torch.cuda.is_available() else "cpu"
+    timesteps=torch.randint(0, self.var_scheduler.steps,(x.shape[0],1),dtype=torch.int64) # sample timesteps
+    
+    noise_gt,eta=self.var_scheduler.add_noise(x,timesteps.cpu())
+    x=x.to(device)
+    y=y.to(device)
+    timesteps=timesteps.to(device)
+    noise_gt=noise_gt.to(device)
+    noise_pred=self.network.forward(noise_gt,timesteps,y)
+
+    mse = nn.MSELoss()
+    loss=mse(noise_pred.to(device),eta.to(device))
+    
+    return loss
+  
+  @torch.no_grad()
+  def generate_sample(self, num_images: int, y, device) -> torch.Tensor:
+    """
+    This method generates as many samples as specified according to the given labels
+    
+    Args:
+    num_images (int): number of images to generate
+    y (_type_): the corresponding expected labels of each image
+    device (_type_): computation device (e.g. torch.device('cuda')) 
+
+    Returns:
+    torch.Tensor: the generated images [num_images, 1, 28, 28]
+    """
+    
+    # add your code here
+    """Given a DDPM model, a number of samples to be generated and a device, returns some newly generated samples"""
+    #timesteps = torch.linspace(0, 200,steps=200,dtype=torch.int64)
+
+    with torch.no_grad():
+          device=device
+
+      # Starting from random noise
+          x = torch.randn(num_images, 1, 28, 28).to(device)
+          
+          for t in range(self.var_scheduler.steps-1,0,-1):
+            # Estimating noise to be removed
+            time_tensor = (torch.ones((num_images,1)) * t).type(torch.int64).to(device)
+            eta_theta = self.network.forward(x, time_tensor,y)
+            
+
+            alpha_t = self.var_scheduler.alphas[t]
+            beta=self.var_scheduler.betas[t]
+            alpha_t_bar = self.var_scheduler.alpha_bars[t]
+            z = torch.randn(num_images, 1, 28, 28).to(device) #noise
+
+            # Partially denoising the image
+            x = (1 / alpha_t.sqrt()) * (x - (beta * eta_theta / (1 - alpha_t_bar).sqrt())) + beta.sqrt()*z
+
+         
+          return x
+
+
+
+def load_diffusion_and_generate():
+  device = torch.device('cuda')
+  var_scheduler = VarianceScheduler() # define your variance scheduler
+  network = NoiseEstimatingNet() # define your noise estimating network
+  diffusion = DiffusionModel(network=network, var_scheduler=var_scheduler) # define your diffusion model
+  
+  # loading the weights of VAE
+  diffusion.load_state_dict(torch.load('diffusion.pt'))
+  diffusion = diffusion.to(device)
+  
+  desired_labels = []
+  for i in range(10):
+    for _ in range(5):
+      desired_labels.append(i)
+
+  desired_labels = torch.tensor(desired_labels).to(device)
+  generated_samples = diffusion.generate_sample(50, desired_labels, device)
+  
+  return generated_samples
---
+++ Diffusion/A5_vae_submission.py
@@ -0,0 +1,174 @@
+import torch
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+"""
+NOTE: you can add as many functions as you need in this file, and for all the classes you can define extra methods if you need
+"""
+
+class VAE(nn.Module):
+	# feel free to define your arguments
+	def __init__(self, hidden_dim=150, latent_dim=300, class_emb_dim=10, num_classes=10):
+		super().__init__()
+		
+		self.encoder = nn.Linear(28*28, hidden_dim, True) # implement your encoder here
+
+		# defining the network to estimate the mean
+		self.mu_net = torch.nn.Linear(hidden_dim, latent_dim, True) # implement your mean estimation module here
+		
+		# defining the network to estimate the log-variance
+		self.logvar_net = torch.nn.Linear(hidden_dim, latent_dim, True) # implement your log-variance estimation here
+		
+		# defining the class embedding module
+		self.class_embedding = torch.nn.Embedding(num_classes, latent_dim) # implement your class-embedding module here
+
+		# defining the decoder here
+		self.decoder = nn.Sequential(nn.Linear(latent_dim,hidden_dim, True), nn.ReLU(),nn.Linear(hidden_dim,28*28, True),
+		nn.Sigmoid())
+
+
+ # implement your decoder here
+
+	def forward(self, x: torch.Tensor, y: torch.Tensor):
+		"""
+		Args:
+				x (torch.Tensor): image [B, 1, 28, 28]
+				y (torch.Tensor): labels [B]
+				
+		Returns:
+				reconstructed: image [B, 1, 28, 28]
+				mu: [B, latent_dim]
+				logvar: [B, latent_dim]
+		"""
+		
+		# implement your forward function here
+		
+
+		
+	# mu=None
+	# logvar = None 
+	# reconstructed=None
+
+		x = x.view(x.size(0), -1)
+		
+		x1 = F.relu(self.encoder(x))
+
+		mu = self.mu_net(x1) # mean 
+		mu= F.relu(mu)
+		mu = F.sigmoid(mu)
+	
+		logvar = self.logvar_net(x1) # log_var 
+		logvar= F.relu(logvar)
+		logvar = F.sigmoid(logvar)
+
+
+
+		x2 = self.reparameterize(mu,logvar)
+
+		#x3 = F.relu(self.fc2(x2))
+
+		#labels=F.one_hot(y,num_classes=10)
+		device = torch.device('cuda')
+
+		#y=y.type(torch.float)
+		#y=y.to(device)
+
+
+
+		embeddings=self.class_embedding(y) # get embeddings 
+		embeddings=embeddings+x2 # add embeddings to samples from reparameterization
+
+		x4 = self.decoder(embeddings)
+		reconstructed = x4.view(x.size(0), 1, 28, 28)
+
+
+		return reconstructed, mu, logvar
+
+	def reparameterize(self, mu: torch.Tensor, logvar: torch.Tensor):
+		"""
+		applies the reparameterization trick
+		"""
+		device = torch.device('cuda')
+
+		new_sample = mu + torch.randn(logvar.shape).to(device) * torch.exp(logvar/2.0) # sampling from normal distribution
+
+		return new_sample
+
+	def kl_loss(self, mu, logvar):
+		"""
+		calculates the KL divergence between a normal distribution with mean "mu" and
+		log-variance "logvar" and the standard normal distribution (mean=0, var=1)
+		"""
+		
+		if torch.cuda.is_available():
+			device=torch.device("cuda")
+		else:
+			device=torch.cuda('cpu')
+			
+		loss_tensor = logvar + mu**2 - torch.log(logvar) - 1.0
+	
+		kl_div = 0.5*(torch.sum(loss_tensor))# calculate the kl-div using mu and logvar
+
+		return kl_div
+
+	def get_loss(self, x: torch.Tensor, y: torch.Tensor):
+		"""
+		given the image x, and the label y calculates the prior loss and reconstruction loss
+		"""
+		labels=y
+		reconstructed, mu, logvar = self.forward(x, labels)
+
+		# reconstruction loss
+		# compute the reconstruction loss here using the "reconstructed" variable above
+		device=torch.device('cuda')
+		#recons_img=self.generate_sample(y.shape[0],y,device)
+		loss=nn.L1Loss()
+		recons_loss = loss(reconstructed,x)
+
+		# prior matching loss
+		prior_loss = self.kl_loss(mu, logvar)
+		return recons_loss, prior_loss
+
+	@torch.no_grad()
+	def generate_sample(self, num_images: int, y, device):
+		"""
+		generates num_images samples by passing noise to the model's decoder
+		if y is not None (e.g., y = torch.tensor([1, 2, 3]).to(device)) the model
+		generates samples according to the specified labels
+		
+		Returns:
+				samples: [num_images, 1, 28, 28]
+		"""
+		
+		# sample from noise, find the class embedding and use both in the decoder to generate new samples
+		
+		
+		
+		labels=self.class_embedding(y)
+		labels+=torch.randn((num_images,1),dtype=float).to(device)
+		samples=self.decoder(labels)
+	
+		samples = samples.view((num_images,1,28,28))
+		
+		return samples
+
+
+def load_vae_and_generate():
+		device = torch.device('cuda')
+		vae = VAE() # define your VAE model according to your implementation above
+		
+		# loading the weights of VAE
+		vae.load_state_dict(torch.load('vae.pt'))
+		vae = vae.to(device)
+		
+		desired_labels = []
+		for i in range(10):
+				for _ in range(5):
+						desired_labels.append(i)
+
+		desired_labels = torch.tensor(desired_labels,torch.float).to(device)
+		generated_samples = vae.generate_sample(50, desired_labels, device)
+		
+		return generated_samples
